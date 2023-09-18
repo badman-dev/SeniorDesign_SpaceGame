f0cf3a7fea2c62c945c4bae128579788745b1bb0
Lucas Thielmann <thielmlw@mail.uc.edu>
2023-09-18 14:26:42 -0400

    started setting up a test scene with player controller
    -imported unity input system package

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class playerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public InputActionReference thrusterAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
