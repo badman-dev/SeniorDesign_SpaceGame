32819b34f4bafc66518f1efb547dd81d88a84d3f
Lucas Thielmann <thielmlw@mail.uc.edu>
2023-10-31 16:32:21 -0400

    tweaked some text fitting settings on the modal

--- Assets/UIModal/Scripts/Demo/DemoManager.cs
+++ Assets/UIModal/Scripts/Demo/DemoManager.cs
@@ -23,7 +23,14 @@
         /// </summary>
         private void ShowModal()
         {
-            ModalManager.Show("Modal Title", "Show your message here", new[] { new ModalButton() { Text = "OK" } });
+            ModalManager.Show("Did You Know??", "The FitnessGram™ Pacer Test is a multistage aerobic" +
+                " capacity test that progressively gets more difficult as it continues. The 20 meter" +
+                " pacer test will begin in 30 seconds. Line up at the start. The running speed starts" +
+                " slowly, but gets faster each minute after you hear this signal. [beep] A single lap" +
+                " should be completed each time you hear this sound. [ding] Remember to run in a straight line," +
+                " and run as long as possible. The second time you fail to complete a lap before the sound," +
+                " your test is over. The test will begin on the word start. On your mark, get ready, start.",
+                new[] { new ModalButton() { Text = "OK" } });
         }
 
         /// <summary>
